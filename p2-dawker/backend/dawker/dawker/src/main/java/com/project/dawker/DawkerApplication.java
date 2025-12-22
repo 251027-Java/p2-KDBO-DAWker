e2531e4f6efb01d0e068fffb913e75bd042a9706
PranavKoduriOSU <pranavkoduriosu@outlook.com>
2025-12-22 13:17:44 -0600

    backend setup

---
+++ p2-dawker/backend/dawker/dawker/src/main/java/com/project/dawker/DawkerApplication.java
@@ -0,0 +1,13 @@
+package com.project.dawker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DawkerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DawkerApplication.class, args);
+	}
+
+}
---
+++ p2-dawker/backend/dawker/dawker/src/test/java/com/project/dawker/DawkerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.dawker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DawkerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
