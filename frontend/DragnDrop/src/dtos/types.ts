d9bcb0abb9609d35cbf5dc86c2f73db24c28a1d6
Kenneth Vo <kennethvo2003@gmail.com>
2025-12-29 10:07:08 -0600

    added temp backend

--- frontend/DragnDrop/src/dtos/types.ts
+++ frontend/DragnDrop/src/dtos/types.ts
@@ -2,12 +2,13 @@
 // 4. The Top-Level DAW State. What is sent out on search
 export interface DawDTO {
   id: string;
-  userId: string;
+  userId: number;
   listOfConfigs: ConfigDTO[];
 }
 
 // 3. The saved list of configurations within a DAW
 export interface ConfigDTO {
+  id?: string;
   name: string;
   componentChain: ComponentDTO[];
 }
@@ -15,8 +16,8 @@ export interface ConfigDTO {
 // 2. The Individual components within the chain. 
 // Order is important here as you decide how the audio signal flows
 export interface ComponentDTO {
-  instanceId: string;
-  modelId: string;
+  id?: string;
+  instanceId: string; // You can have many of one component, they should have unique ids within the chain
   name: string;
   type: 'filter' | 'reverb' | 'distortion' | 'delay'; // Literal types prevent typos
   settings: SettingsDTO;
@@ -28,6 +29,7 @@ export interface ComponentDTO {
 // export_names are to help decide what Device within RNBO you are working with
 // Basically, individual settings for each component
 export interface SettingsDTO {
+    id?: string;
     Technology: 'RNBO' | 'TONEJS';
     export_name: string;
     parameters: Record<string, number | string | boolean>;
