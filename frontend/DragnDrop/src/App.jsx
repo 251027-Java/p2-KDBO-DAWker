ae7937c85a7c0da036ca6fe3d4fac8625fdefaf1
Kenneth Vo <kennethvo2003@gmail.com>
2025-12-31 11:01:59 -0600

    finished up moving tonejs over, making it work with types.ts

--- frontend/DragnDrop/src/App.jsx
+++ frontend/DragnDrop/src/App.jsx
@@ -1,10 +1,15 @@
 import DragDrop from './components/DragDrop'
+import TonejsDemo from './components/TonejsDemo'
 import './App.css'
 
 function App() {
   return (
     <main>
-      <DragDrop />
+      <div className="app">
+        <TonejsDemo />
+      </div>
+      {/* for now i uncommented the dragdrop component i just didn't know what to do with it */}
+      {/* <DragDrop /> */}
     </main>
   )
 }
